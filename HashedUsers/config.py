7140786a7f9364763a4667413b95e74556c86f9d
FlamingForge <perickson@student.neumont.edu>
2025-08-13 11:47:52 -0600

    changed salt

--- HashedUsers/config.py
+++ HashedUsers/config.py
@@ -1,4 +1,4 @@
 SECRET_KEY = "hardcoded-dev-secret-123"
 
 MONGO_URI = "mongodb+srv://admin:admin@supersecurepasswords.9s5oxox.mongodb.net/?retryWrites=true&w=majority&appName=SuperSecurePasswords"
-GLOBAL_SALT = "reused-salt-please-dont"
+GLOBAL_SALT = "SaltIsDelisious"
